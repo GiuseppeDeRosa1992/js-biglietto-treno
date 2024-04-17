132eb160b1e0c7f61ebc198f7f715cb31687471f
GiuseppeDeRosa1992 <derosagiuseppe1092@gmail.com>
2024-04-17 17:13:50 +0200

    added bonus print price inside html

--- js/main.js
+++ js/main.js
@@ -9,11 +9,17 @@ const age = prompt("Inserisci età");
 let priceTicket = priceKm * distanceKm;
 console.log(`Il prezzo del biglietto sarà: ${priceTicket}`)
 
+// Stringa
+string = "Il prezzo normale è:"
+stringJunior = "Il prezzo scontato per i minorenni è:"
+stringSenior = "Il prezzo scontato per i senior è:"
+
 // Sconti
 
 if (age<=17) {
     priceTicketJunior = ((priceKm * distanceKm) - (priceTicket * 20 / 100));
     console.log(`Il prezzo del biglietto per i minorenni sarà: ${priceTicketJunior}`);
+    document.getElementById("price-junior").innerHTML = stringJunior + priceTicketJunior;
 }
 
 
@@ -21,5 +27,7 @@ if (age<=17) {
 if (age>=65) {
     priceTicketSenior = ((priceKm * distanceKm) - (priceTicket * 40 / 100));
     console.log(`Il prezzo del biglietto per i senior sarà: ${priceTicketSenior}`);
+    document.getElementById("price-senior").innerHTML = stringSenior + priceTicketSenior;
 }
 
+document.getElementById("price").innerHTML = string + priceTicket;
