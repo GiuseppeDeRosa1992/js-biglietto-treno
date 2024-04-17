eb13a02b0a4a93e5f869756999d7ec3bd9a4ef20
GiuseppeDeRosa1992 <derosagiuseppe1092@gmail.com>
2024-04-17 17:02:32 +0200

    added price with sale and if

--- js/main.js
+++ js/main.js
@@ -1,9 +1,25 @@
 // Tariffa per KM
-const priceKm = 0.21
+const priceKm = 0.21;
 
 // Distanza in km che percorre da chiedere all'utente ed età
-const distanceKm = prompt("Inserisci la distanza che vuoi percorrere")
-const age = prompt("Inserisci età")
+const distanceKm = prompt("Inserisci la distanza che vuoi percorrere");
+const age = prompt("Inserisci età");
 
 // Prezzo del biglietto senza sconto
-priceTicket = priceKm * distanceKm
+let priceTicket = priceKm * distanceKm;
+console.log(`Il prezzo del biglietto sarà: ${priceTicket}`)
+
+// Sconti
+
+if (age<=17) {
+    priceTicketJunior = ((priceKm * distanceKm) - (priceTicket * 20 / 100));
+    console.log(`Il prezzo del biglietto per i minorenni sarà: ${priceTicketJunior}`);
+}
+
+
+
+if (age>=65) {
+    priceTicketSenior = ((priceKm * distanceKm) - (priceTicket * 40 / 100));
+    console.log(`Il prezzo del biglietto per i senior sarà: ${priceTicketSenior}`);
+}
+
