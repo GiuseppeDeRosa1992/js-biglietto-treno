a8a2f6acf5464dbd61c4f5308e02d19840a63f43
GiuseppeDeRosa1992 <derosagiuseppe1092@gmail.com>
2024-04-17 15:37:13 +0200

    added price ticket inside console without sale

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// Tariffa per KM
+const priceKm = 0.21
+
+// Distanza in km che percorre da chiedere all'utente ed età
+const distanceKm = prompt("Inserisci la distanza che vuoi percorrere")
+const age = prompt("Inserisci età")
+
+// Prezzo del biglietto senza sconto
+priceTicket = priceKm * distanceKm
