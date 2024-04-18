80ed5d89642f1d3bcb121aa30cd5f87497581fe5
GiuseppeDeRosa1992 <derosagiuseppe1092@gmail.com>
2024-04-18 14:29:16 +0200

    improved js code

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ const age = prompt("Inserisci età");
 // Prezzo del biglietto senza sconto
 let priceTicket = priceKm * distanceKm;
 priceFixed = priceTicket.toFixed(2);
-console.log(`Il prezzo del biglietto sarà: ${priceFixed}`)
+console.log(`Il prezzo base del biglietto è: ${priceFixed}`)
 
 // Stringa
 string = "Il prezzo normale è:"
@@ -18,18 +18,18 @@ stringSenior = "Il prezzo scontato per i senior è:"
 // Sconti
 
 if (age<=17) {
-    priceTicketJunior = ((priceKm * distanceKm) - (priceTicket * 20 / 100));
+    priceTicketJunior = ((priceTicket) - (priceTicket * 0.2));
     priceTicketJuniorFixed = priceTicketJunior.toFixed(2)
-    console.log(`Il prezzo del biglietto per i minorenni sarà: ${priceTicketJuniorFixed}`);
+    console.log(`Il prezzo del biglietto scontato per i minorenni sarà: ${priceTicketJuniorFixed}`);
     document.getElementById("price-junior").innerHTML = stringJunior + priceTicketJuniorFixed;
 }
 
 
 
-if (age>=65) {
-    priceTicketSenior = ((priceKm * distanceKm) - (priceTicket * 40 / 100));
+else if (age>=66) {
+    priceTicketSenior = ((priceTicket) - (priceTicket * 0.4));
     priceTicketSeniorFixed = priceTicketSenior.toFixed()
-    console.log(`Il prezzo del biglietto per i senior sarà: ${priceTicketSeniorFixed}`);
+    console.log(`Il prezzo del biglietto scontato per i senior sarà: ${priceTicketSeniorFixed}`);
     document.getElementById("price-senior").innerHTML = stringSenior + priceTicketSeniorFixed;
 }
 
